93a919ede39404865cc99b049036069cdcf4626a
kevindotklein <kevindotklein@gmail.com>
2024-09-19 23:37:42 -0300

    docs: main loop and handle input

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from common.defs import *
 
 class Game:
     def __init__(self, angle = 90) -> None:
+        "init game stuff"
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Asteroids")
@@ -16,14 +17,8 @@ class Game:
         self.spaceship_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
         self.angle = angle
 
-    def run(self) -> None:
-        while self.running:
-            self.clock.tick(60)
-            
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-
+    def handle_input(self) -> None:
+            "handle keyboard input"
             keys = pygame.key.get_pressed()
             if keys[K_w]:
                 self.spaceship_pos.x += math.cos(math.radians(self.angle)) * SPACESHIP_SPEED
@@ -32,7 +27,17 @@ class Game:
                 self.angle = (self.angle + 5) % 360 
             if keys[K_d]:
                 self.angle = (self.angle - 5) % 360
-                
+
+    def run(self) -> None:
+        "main game loop"
+        while self.running:
+            self.clock.tick(60)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.handle_input()
                 
             self.screen.fill("black")
 

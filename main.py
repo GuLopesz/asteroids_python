2b32b7ab134c626a0f6efe81468bcecb92c5b37b
kevindotklein <kevindotklein@gmail.com>
2024-09-20 10:36:57 -0300

    feat: kinda asteroids impl

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from pygame.locals import *
 from entities.spaceship import *
@@ -20,6 +21,7 @@ class Game:
 
         self.player: Spaceship = Spaceship()
         self.lasers: List[Laser] = []
+        self.asteroid = self.gen_random_polygon(7, (100, 100), 65, 30)
 
     def handle_input(self) -> None:
         "handle game input"
@@ -28,6 +30,7 @@ class Game:
         if keys[K_SPACE] and self.laser_tick == 0:
             self.lasers.append(Laser(self.player.pos, self.player.angle))
             self.can_laser = False
+            self.asteroid = self.gen_random_polygon(7, (100, 100), 65, 30)
     
     def clear_lasers(self) -> None:
         "delete lasers that outbounds the screen"
@@ -35,13 +38,28 @@ class Game:
             if l.pos.x >= SCREEN_WIDTH or l.pos.x < 0 or l.pos.y >= SCREEN_HEIGHT or l.pos.y < 0:
                 self.lasers.remove(l)
 
+    def gen_random_polygon(self, sides, center, radius, noise) -> list:
+        points = []
+        angle = 0
+        for _ in range(sides):
+            x = center[0] + (radius + random.uniform(-noise, noise)) * math.cos(angle)
+            y = center[1] + (radius + random.uniform(-noise, noise)) * math.sin(angle)
+            points.append((x, y))
+            angle += 2 * math.pi / sides
+        return points
     
+    def render_asteroids(self) -> None:
+        pygame.draw.polygon(self.screen, BLUE, self.asteroid, 0)
+
     def render(self) -> None:
         "render stuff"
         for l in self.lasers:
             l.render(self.screen)
 
         self.player.render(self.screen)
+
+        self.render_asteroids()
+
         pygame.display.flip()
 
     def run(self) -> None:
@@ -58,8 +76,8 @@ class Game:
             self.player.update()
             for l in self.lasers:
                 l.update()
-                
-            self.screen.fill("black")
+
+            self.screen.fill(BLACK)
             self.render()
             self.clear_lasers()
             if self.can_laser == False:
